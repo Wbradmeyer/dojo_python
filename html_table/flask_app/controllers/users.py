af49ddb54c2a94ab3ae278e33fb4873f346df299
Wbradmeyer <107085402+Wbradmeyer@users.noreply.github.com>
2023-08-16 21:25:16 -0400

    Add files via upload

---
+++ html_table/flask_app/controllers/users.py
@@ -0,0 +1,17 @@
+from flask_app import app
+from flask import render_template
+from flask_app.models import user
+
+
+@app.route('/')
+def render_table():
+
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+    return render_template('index.html', users = users)
+
